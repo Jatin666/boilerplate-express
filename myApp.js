2a3a0265a80943105ab3f672f0b41c93d10ae491
Jatin666 <verma.jatin6661@gmail.com>
2022-10-03 11:22:33 +0200

    using of .env filr using its variable for using get request to print in upper case. Freecodecamp

--- myApp.js
+++ myApp.js
@@ -1,7 +1,26 @@
 let express = require('express');
 let app = express();
+let absolutePath = __dirname + "/views/index.html";
+require('dotenv').config();
+// app.get("/", (req, res)=> {
+//     res.send("Hello World")
+//     });
+    
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res)=> {  //whenever this block will run this will go through the app.use
+res.sendFile(absolutePath)
+});
+
+app.get("/json", function(req, res){
+    var jsonResponse = { "message":"Hello json" };
+    if (process.env.MESSAGE_STYLE === "uppercase"){
+        jsonResponse.message = jsonResponse.message.toUpperCase()}
+        res.json(jsonResponse);
+});
 
 
+console.log("Hello World");
 
 
 
