30e945bd24fce7b4166e96a69d06832098f208f7
Jatin666 <verma.jatin6661@gmail.com>
2022-10-03 11:32:50 +0200

    freecodecamp Implement a Root-Level Request Logger Middleware

--- myApp.js
+++ myApp.js
@@ -6,12 +6,22 @@ require('dotenv').config();
 //     res.send("Hello World")
 //     });
     
+//freecodecamp Implement a Root-Level Request Logger Middleware
+app.use(function (req, res, next){
+    console.log(req.method + " " + req.path + " - " +req.ip)
+    next();
+});
+
+
+
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res)=> {  //whenever this block will run this will go through the app.use
 res.sendFile(absolutePath)
 });
 
+
+//freecodecamp use of .env file 
 app.get("/json", function(req, res){
     var jsonResponse = { "message":"Hello json" };
     if (process.env.MESSAGE_STYLE === "uppercase"){
@@ -19,7 +29,7 @@ app.get("/json", function(req, res){
         res.json(jsonResponse);
 });
 
-
+//freecodecamp use of console.log
 console.log("Hello World");
 
 
